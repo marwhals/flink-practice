4f5a449338f2f506ab49938c78389bc3d52156e9
marwhals <marjan.mubarok2012@gmail.com>
2025-09-16 09:55:42 +0100

    Triggers

---
+++ src/main/scala/datastreams/Triggers.scala
@@ -0,0 +1,72 @@
+package datastreams
+
+import generators.shopping.{ShoppingCartEvent, ShoppingCartEventsGenerator}
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.function.ProcessAllWindowFunction
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.triggers.{CountTrigger, PurgingTrigger}
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+/**
+ * Triggers
+ * - Specify when a window function will be executed
+ * ---- Can run multiple times on the window
+ * ---- Has nothing to do with the window, i.e event grouping
+ * - Purging triggers: clear the contents of the window after the trigger fires
+ * --- wraps another trigger
+ * - Other triggers exist: Event time / processing time (automatic), custom
+ */
+
+object Triggers {
+
+  // Triggers - determine when a window function is executed
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  def demoCountTrigger(): Unit = {
+    val shoppingCartEvents: DataStream[String] = env
+      .addSource(new ShoppingCartEventsGenerator(500, 2)) // Batch size: two events per second
+      .windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5))) // 20 events per window
+      .trigger(CountTrigger.of[TimeWindow](5)) // the window function runs every 5 elements
+      .process(new CountByWindowAll)
+
+    shoppingCartEvents.print()
+    env.execute()
+
+  }
+
+  // purging trigger - clears the window when it fires
+
+  def demoPurgingTrigger(): Unit = {
+    val shoppingCartEvents: DataStream[String] = env
+      .addSource(new ShoppingCartEventsGenerator(500, 2)) // 2 events/second
+      .windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5))) // 10 events/window
+      .trigger(PurgingTrigger.of(CountTrigger.of[TimeWindow](5))) // the window function runs every 5 elements, THEN CLEARS THE WINDOW
+      .process(new CountByWindowAll) // runs twice for the same window
+
+    shoppingCartEvents.print()
+    env.execute()
+  }
+
+  /**
+  Other triggers:
+  - EventTimeTrigger - happens by default when the watermark is greater than the window end time (automatic for event time windows)
+  - ProcessingTimeTrigger - fires when the current system time greater than the window end time (automatic for processing time windows)
+  - custom triggers - powerful APIs for custom firing behavior
+ */
+
+  def main(args: Array[String]): Unit = {
+    demoCountTrigger()
+  }
+
+  //A demo function for triggers
+  class CountByWindowAll extends ProcessAllWindowFunction[ShoppingCartEvent, String, TimeWindow] {
+    override def process(context: Context, elements: Iterable[ShoppingCartEvent], out: Collector[String]): Unit = {
+      val window = context.window
+      out.collect(s"Window [${window.getStart} - ${window.getEnd}] ${elements.size}")
+    }
+  }
+
+}
